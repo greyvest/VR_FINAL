06b66151ff41c2b921f6a098621e4dd11f956a43
unknown <tim.taznar@gmail.com>
2019-10-17 19:29:14 -0600

    Added Initial NavMesh for units

---
+++ Assets/Scripts/Units/Controls/ShipNavigation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShipNavigation : MonoBehaviour
+{
+
+    [SerializeField]
+    NavMeshAgent agent;
+
+    Vector3 TargetPos;
+    bool fighting = false;
+
+    public void TravelTo(Vector3 point)
+    {
+        //TargetPos = point;
+        agent.SetDestination(point);
+    }
+
+
+    private IEnumerator PauseBeforeKill(GameObject enemy)
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(enemy);
+        fighting = false;
+        agent.isStopped = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with: " + other.name);
+        if(!fighting && other.tag == "Enemy")
+        {
+            fighting = true;
+            agent.isStopped = true;
+            StartCoroutine(PauseBeforeKill(other.gameObject));
+        }
+    }
+}
