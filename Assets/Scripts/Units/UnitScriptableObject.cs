113ee139c591e0105678e35238dde09a655143b6
unknown <tim.taznar@gmail.com>
2019-10-17 23:26:54 -0600

    Adjusted PlayerNavigation

---
+++ Assets/Scripts/Units/UnitScriptableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Unit", order = 1)]
+public class UnitScriptableObject : ScriptableObject
+{
+    public string UnitName;
+
+    public float MaxHealth;
+    public float Damage;
+    public float Speed;
+    public float Range;
+}
